48efd24fab620be5bf921a8967aa973b1a4068a5
gwt805 <1973735972@qq.com>
2023-03-04 00:26:24 +0800

    update tutorials

--- Community/main.js
+++ Community/main.js
@@ -1,11 +1,11 @@
-const { app, BrowserWindow, Menu, Tray, dialog } = require('electron')
+const { app, BrowserWindow, Menu, Tray, globalShortcut } = require('electron')
 const path = require("path")
 
 let tray
 let mainWindow
 
 const gotTheLock = app.requestSingleInstanceLock();
-if (!gotTheLock) app.quit()
+if (!gotTheLock) app.exit()
 else {
     function createWindow() {
         Menu.setApplicationMenu(null) // 隐藏菜单栏
@@ -21,51 +21,33 @@ else {
 
         // mainWindow.loadFile('./ng-cc/index.html') // 将本地文件打包
         mainWindow.maximize(); // 打开时最大化
+
+        mainWindow.on('close', e => {
+            e.preventDefault();
+            mainWindow.hide();
+        })
     }
-    
-    app.on('close', (e) => {
-        e.preventDefault();
-        mainWindow.hide();
-        mainWindow.setSkipTaskbar(true);
+
+    app.on('ready',() => {
+        globalShortcut.register('Ctrl+Shift+I', function () {
+            mainWindow.webContents.openDevTools();
+        })
+        createWindow();
     })
 
-    app.on('closed', () => {
-        mainWindow = null;//移除相应窗口的引用对象，避免再次使用它.
-    })
-
-    app.on('ready', createWindow)
     app.on('activate', () => {
         if (mainWindow === null) {
             createWindow()
         }
     })
 
-    app.on('window-all-closed', (e) => {
-        createWindow();
-        dialog.showMessageBox({
-            type: 'info',
-            title: '提示',
-            message: '确认退出？',
-            buttons: ['直接退出', '退到系统托盘'],
-            cancelId: 1,
-        }).then(idx => {
-            if (idx.response == 1) {
-                e.preventDefault();
-                mainWindow.hide();
-            } else {
-
-                app.exit();
-            }
-        }).catch(err => { })
-    })
-
     app.whenReady().then(() => {
         tray = new Tray(path.join(__dirname, 'favicon.png'))
         const contextMenu = Menu.buildFromTemplate([
             { label: '显示', click: () => { mainWindow.show() } },
-            { label: '退出', click: () => { app.quit() } }
+            { label: '退出', click: () => { app.exit() } }
         ])
-        tray.setToolTip('Community')
+        tray.setToolTip('ESS')
         tray.setContextMenu(contextMenu)
     })
 
--- README.md
+++ README.md
@@ -8,12 +8,20 @@
 
 
 # 注意事项
+- 每次打包的时候都需要删除前一次的 Cache
+    - Windows
+        - 删除 C:\Users\xx\AppData\Local\electron-builder\Cache\ 下的所有内容
+        - 删除 C:\Users\gs\AppData\Local\electron\Cache\ 下的所有内容
+    - Linux
+        - 删除 ~/.cache/electron/*
+        - 删除 ~/.cache/electron-builder/*
+
+- 每次 打包时，都需要清空 dist文件夹里的内容，或者直接删除 dist 文件夹
+
 - 各平台打包 icon 
     - | Winodws     | MacOS       | Linux     |
       | ----------- | ----------- | --------- |
       | .ico        | .icns       |.png       |
-
-
 
 - main.js
     - tray.setToolTip('xxx') 这里要修改成 你想要的提示，即鼠表放在任务栏图标上的提示，不过好像没生效？具体可实验，生效的可能是 package.json 中的 description
@@ -25,13 +33,11 @@
 
     - description: 即 打包后鼠标放在任务栏图标上显示的文字
 
-    - 打包时，需要将 dependencies 中的内容清空，把本文件挪走，不然会连这个文件一起打包进去的
+    - 打包时，需要将 dependencies 中的内容清空
 
     - Linux 打包时，需要对 author 中gwt 后面添加 邮箱：Inc <xxx@xx.com>
 
     - windows 打包时，author 中 只保留 gwt 即可，另 author 和 build 中的appID 的 com.example.xx xx 同名
-
-    - 每次 打包时，都需要清空 dist文件夹里的内容，或者直接删除 dist 文件夹
 
 
 # 打包教程
